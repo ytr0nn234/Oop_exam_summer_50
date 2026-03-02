d80093bfccb2fa7535a6d04f13b0b4f6370bd199
ytr0nn234 <g00467242@atu.ie>
2026-03-02 11:29:30 +0000

    Initial classes

---
+++ src/main/java/com/example/oop_exam_summer_50/BookBase.java
@@ -0,0 +1,22 @@
+package com.example.oop_exam_summer_50;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import lombok.Setter;
+
+
+
+public class BookBase {
+
+
+    private int id;
+    private String Tag;
+    private String studentMail;
+    private String reservationDate;
+    private String startHour;
+    private String durationHour;
+
+
+
+}
---
+++ src/main/java/com/example/oop_exam_summer_50/BookService.java
@@ -0,0 +1,6 @@
+package com.example.oop_exam_summer_50;
+
+public class BookService {
+
+
+}
