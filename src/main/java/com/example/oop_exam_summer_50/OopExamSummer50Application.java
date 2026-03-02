842754d4da65987a5eeef06cfc1483a7e8b275e2
ytr0nn234 <g00467242@atu.ie>
2026-03-02 11:10:54 +0000

    Initial commit

---
+++ src/main/java/com/example/oop_exam_summer_50/OopExamSummer50Application.java
@@ -0,0 +1,13 @@
+package com.example.oop_exam_summer_50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopExamSummer50Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopExamSummer50Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oop_exam_summer_50/OopExamSummer50ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oop_exam_summer_50;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopExamSummer50ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
